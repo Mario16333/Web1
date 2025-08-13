1e1f8f675e97af9aab91dda9ab0d304b40ecdc23
Mario16333 <mariocarrera163@gmail.com>
2025-08-12 21:58:18 -0500

    🕐 Fix fecha: usar timestamp local sin conversión de zona horaria

--- server.py
+++ server.py
@@ -325,10 +325,9 @@ def get_file_info(filename):
         file_size = stat_info.st_size
         mod_time = datetime.fromtimestamp(stat_info.st_mtime)
         
-        # Formatear fecha con zona horaria local
-        # Convertir a zona horaria local (UTC-5 para Colombia)
-        local_time = mod_time.replace(tzinfo=timezone.utc).astimezone(timezone(timedelta(hours=-5)))
-        formatted_date = local_time.strftime('%d/%m/%Y, %H:%M:%S')
+        # Formatear fecha - usar la hora local del archivo sin conversión
+        # El timestamp del archivo ya está en hora local del sistema donde se creó
+        formatted_date = mod_time.strftime('%d/%m/%Y, %H:%M:%S')
         
         # Formatear tamaño
         if file_size < 1024:
